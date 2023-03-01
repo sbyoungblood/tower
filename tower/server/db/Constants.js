d2026f33d8059a1a184485e38453e07be41814fa
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-02-28 22:41:06 -0700

    event model

---
+++ tower/server/db/Constants.js
@@ -0,0 +1,3 @@
+export const defaultSchemaOptions = { timestamps: true, toJSON: { virtuals: true } }
+
+export const basicStringType = { type: String, required: true, minLength: 3, maxLength: 500 }
---
+++ tower/server/models/Event.js
@@ -0,0 +1,22 @@
+import { Schema } from "mongoose";
+import { basicStringType, defaultSchemaOptions } from "../db/Constants";
+
+export const EventSchema = new Schema({
+  id: basicStringType,
+  name: basicStringType,
+  description: basicStringType,
+  coverImg: { ...basicStringType, maxLength: 5000 },
+  location: basicStringType,
+  capacity: { type: Number, default: 0 },
+  startDate: { type: Date, required: true },
+  isCanceled: { type: Boolean, default: false },
+  type: { ...basicStringType, enum: ['concert', 'convention', 'sport', 'digital'], default: 'other' },
+  creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
+}, defaultSchemaOptions)
+
+EventSchema.virtual('creator', {
+  ref: 'Account',
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true
+})
